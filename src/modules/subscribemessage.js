a651e89fd619b13555085bb42638f5c05c348cbe
rojerchen <rojerchen@qq.com>
2020-01-01 23:55:23 +0800

    subscribeMessage

---
+++ src/modules/subscribemessage.js
@@ -0,0 +1,8 @@
+module.exports = {
+  addTemplate: '/wxaapi/newtmpl/addtemplate',
+  deleteTemplate: '/wxaapi/newtmpl/deltemplate',
+  getCategory: '/wxaapi/newtmpl/getcategory',
+  getPubTemplateKeyWordsById: '/wxaapi/newtmpl/getpubtemplatekeywords',
+  getPubTemplateTitleList: '/wxaapi/newtmpl/getpubtemplatetitles',
+  send: '/cgi-bin/message/subscribe/send',
+};
